6f9735c8690384959a64d9965273465a2c7710f9
Jacob <jacob.echeverry13@gmail.com>
2024-12-09 00:48:13 -0500

    fixed issue in which header renders twice

--- 3sp/src/app/User/Layouts/CourseLayout.tsx
+++ 3sp/src/app/User/Layouts/CourseLayout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import "../../global.css"; // Adjust the path if necessary
-import Header from "../Components/HeaderUser"; // Import the new Header component
 import { Inter } from "next/font/google"; // Centralized font configuration
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,21 +11,9 @@ export default function CourseLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <body className={inter.className}>
-        <div className="min-h-screen flex flex-col">
-          {/* Reusable Header Component */}
-          <Header
-          // additionalLinks={[
-          //   { label: "Dashboard", href: "/dashboard" },
-          //   { label: "My Courses", href: "/user/courses" },
-          // ]}
-          />
-
-          {/* Main Content */}
-          <main className="flex-grow">{children}</main>
-        </div>
-      </body>
-    </>
+    <div className={`min-h-screen flex flex-col ${inter.className}`}>
+      {/* Main Content */}
+      <main className="flex-grow">{children}</main>
+    </div>
   );
 }
